167eee61f9d09ffe1a8627398111a27946f5123d
chux <ugooaghanya98@gmail.com>
2023-06-06 13:04:13 +0100

    initial commit

---
+++ quiz-project/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Question(models.Model):
+    content = models.TextField(unique=True)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    correct_option = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.pk
