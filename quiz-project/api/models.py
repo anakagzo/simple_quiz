d4ba0e5d0c00cd4dad9a1dc74ce2c47352a0bc42
chux <ugooaghanya98@gmail.com>
2023-06-11 17:07:24 +0100

    Added seventh_ans and eighth_ans to the Quiz model run migrations

--- quiz-project/api/models.py
+++ quiz-project/api/models.py
@@ -14,14 +14,17 @@ class Question(models.Model):
 
 
 class Quiz(models.Model):
-    username = models.CharField(max_length=100,unique=True)
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
-    first_ans = models.CharField(max_length=100,null=True)
-    second_ans = models.CharField(max_length=100,null=True)
-    third_ans = models.CharField(max_length=100,null=True)
-    fourth_ans = models.CharField(max_length=100,null=True)
-    fifth_ans = models.CharField(max_length=100,null=True)
-    sixth_ans = models.CharField(max_length=100,null=True)
+    first_ans = models.CharField(max_length=100, null=True)
+    second_ans = models.CharField(max_length=100, null=True)
+    third_ans = models.CharField(max_length=100, null=True)
+    fourth_ans = models.CharField(max_length=100, null=True)
+    fifth_ans = models.CharField(max_length=100, null=True)
+    sixth_ans = models.CharField(max_length=100, null=True)
+    seventh_ans = models.CharField(max_length=100, null=True)
+    eighth_ans = models.CharField(max_length=100, null=True)
 
     def __str__(self) -> str:
         return str(self.username)
+    
